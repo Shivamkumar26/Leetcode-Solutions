668bc37b42351698903946ca4f6d45eab7224901
Shivam kumar <99862619+Shivamkumar26@users.noreply.github.com>
2022-10-03 13:42:10 +0530

    Time: 416 ms (16.34%), Space: 109.1 MB (5.02%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
@@ -1,37 +1,14 @@
 class Solution {
-    /* Correct but memoization will give TLE */
-    int solve(string colors, vector<int>& time, int idx, int prev, vector<vector<int>>& dp) {
-        
-        if(idx >= colors.size()) return 0;
-        if(dp[idx][prev+1]!=-1) return dp[idx][prev+1];
-        
-        int p1 = INT_MAX, p2 = INT_MAX;
-        if(prev == -1 || colors[prev] != colors[idx]) {
-            p1 = solve(colors, time, idx+1, idx, dp);
-        }
-        p2 = time[idx] + solve(colors, time, idx+1, prev, dp);
-        return dp[idx][prev+1] = min({p1, p2});
-    }
 public:
-    int minCost(string colors, vector<int>& time) {
-        int n=colors.size();
-        vector<vector<int>> dp(n, vector<int>(27, 0));
-        
-        for(int i=0; i<27; i++) 
-            if(colors[0]-'a'==i) dp[0][i] = time[0];
-        
-        for(int i=1; i<n; i++)
-            for(int j=0; j<27; j++) {
-                int p1 = INT_MAX, p2 = INT_MAX;
-                int idx = colors[i]-'a';
-                
-                if(idx != j)
-                    p1 = dp[i-1][idx];
-                p2 = time[i] + dp[i-1][j];
-                
-                dp[i][j] = min(p1, p2);
-            }
-        
-        return dp[n-1][26];
+    int minCost(string colors, vector<int>& neededTime) {
+        int n = colors.size();
+        vector<int> dp(n, -1);
+        return f(n - 1, colors, neededTime, dp, 'A', neededTime[n - 1]);
+    }
+    int f(int i, string& c, vector<int>& t, vector<int>& dp, char prevc, int prevt) {
+        if(i < 0) return 0;
+        if(dp[i] != -1) return dp[i];
+        if(c[i] == prevc) return dp[i] = f(i - 1, c, t, dp, c[i], max(t[i], prevt)) + min(t[i], prevt);
+        else return dp[i] = f(i - 1, c, t, dp, c[i], t[i]);
     }
 };
