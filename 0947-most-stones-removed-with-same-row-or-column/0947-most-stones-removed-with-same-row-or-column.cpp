f331aa071b3209d35cc03f1ddc1217261e10becc
Shivam kumar <99862619+Shivamkumar26@users.noreply.github.com>
2022-11-14 16:28:38 +0530

    Time: 543 ms (24.27%), Space: 14.1 MB (88.33%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -2,17 +2,18 @@ class Solution {
     void dfs(vector<vector<int>>& stones, vector<bool>& visited, int ind, int n) {
         visited[ind] = true;
         for(int i=0; i<n; i++)
+            // if any row or col has same i, ind val that means they are in same row so call dfs and make them visited as well.
             if(!visited[i] && (stones[ind][0]==stones[i][0] || stones[ind][1]==stones[i][1])) 
                 dfs(stones, visited, i, n);
     }
 public:
     int removeStones(vector<vector<int>>& stones) {
         int n = stones.size(), ans = n;
-        vector<bool> visited(n, false);
+        vector<bool> visited(n, false); 
         
         for(int i = 0; i < n; i++) {
-            if(!visited[i]) {
-                dfs(stones, visited, i, n);
+            if(!visited[i]) { // visited array
+                dfs(stones, visited, i, n); // counting island and reducing ans
                 ans--;
             }
         }
