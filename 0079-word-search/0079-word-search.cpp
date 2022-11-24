8b9c4cf29c4a8427febcebb2fd339d88b4f6a6ea
Shivam kumar <99862619+Shivamkumar26@users.noreply.github.com>
2022-11-24 19:38:49 +0530

    Time: 1509 ms (18.34%), Space: 8 MB (79.86%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,43 +1,30 @@
 class Solution {
-    bool solve(vector<vector<char>>& board, string word, int r, int c) {
-        if(word.size()==0) return true;
-        if(r<0 || r==board.size() || c<0 || c==board[0].size() || word[0]!=board[r][c])
+    bool searchword(int r, int c, vector<vector<char>>& board, string word) {
+        if(word.size() == 0) return true;
+        int n = board.size(), m = board[0].size();
+        if(r<0 || c<0 || r>=n || c>=m || board[r][c]!=word[0])
             return false;
         
-        // move in 4 dir 
         board[r][c] = '#';
-        string s = word.substr(1, word.size()-1);
-        
-        bool t =  solve(board, s, r+1, c) || solve(board, s, r-1, c) || 
-                solve(board, s, r, c+1) || solve(board, s, r, c-1);
+        bool ans = false;
+        string wordd = word.substr(1, word.size()-1);
+        ans = searchword(r+1, c, board, wordd) | 
+            searchword(r, c+1, board, wordd) |
+            searchword(r-1, c, board, wordd) | 
+            searchword(r, c-1, board, wordd);
+            
         board[r][c] = word[0];
-        return t;
+        return ans;
     }
 public:
     bool exist(vector<vector<char>>& board, string word) {
         int n = board.size(), m = board[0].size();
-        for(int i = 0; i < n; i++) 
-            for(int j = 0; j < m; j++) 
-                if(solve(board, word, i, j)) return true;
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                if(searchword(i, j, board, word)) 
+                    return true;
+            }
+        }
         return false;
     }
 };
-
-/*
-
-a b c e
-s f e s
-a d e e 
-
-a b c e s e e e f s 
-
-
-when using visited array the prob of tle and WA in these TCs
-
-[["A","A","A","A","A","A"],["A","A","A","A","A","A"],["A","A","A","A","A","A"],["A","A","A","A","A","A"],["A","A","A","A","A","A"],["A","A","A","A","A","A"]]
-"AAAAAAAAAAAAAAB"
-
-[["A","B","C","E"],["S","F","E","S"],["A","D","E","E"]]
-"ABCESEEEFS"
-
-*/
