bb8e448bd549da7413900e29e7bb82f5e5c7f6be
Shivam kumar <99862619+Shivamkumar26@users.noreply.github.com>
2022-12-17 13:52:15 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (86.39%) - LeetHub

--- 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.cpp
+++ 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.cpp
@@ -1,11 +1,8 @@
 class Solution {
-    int s=0, c=1;
+    int s=1;
 public:
     int twoEggDrop(int n) {
-        if(n<=0) return 0;
-        s=c;
-        c++;
-        return 1+twoEggDrop(n-s);
+        return (n<=0) ? 0 : 1+twoEggDrop(n-s++);
     }
 };
 /**
