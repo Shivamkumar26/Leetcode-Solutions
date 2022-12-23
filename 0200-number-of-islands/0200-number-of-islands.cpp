9a53eae7907e8c5de19ead4da0cebbb9740d1f4b
Shivam kumar <99862619+Shivamkumar26@users.noreply.github.com>
2022-12-23 22:47:06 +0530

    Time: 67 ms (65.74%), Space: 19.2 MB (13.63%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -0,0 +1,43 @@
+class Solution {
+    void bfs(int r, int c, vector<vector<int>>& vis, vector<vector<char>>& grid) {
+        queue<pair<int,int>> q;
+        int n = grid.size();
+        int m = grid[0].size();
+        
+        q.push({r,c});
+        vis[r][c] = 1;
+        
+        while(!q.empty()) {
+            int r = q.front().first;
+            int c = q.front().second;
+            q.pop();
+            
+            int dr[] = {1, -1, 0, 0};
+            int dc[] = {0, 0, -1, 1};
+            for(int i=0; i<4; i++) {
+                int nr = r + dr[i];
+                int nc = c + dc[i];
+                if (nr >= 0 && nc >= 0 && nr < n && nc < m 
+                && grid[nr][nc] == '1' && vis[nr][nc] == 0) {
+                    vis[nr][nc]=1;
+                    q.push({nr, nc});
+                }
+            }
+        }
+    }
+  public:
+    int numIslands(vector<vector<char>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        int ans = 0;
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        
+        for(int i = 0; i<n; i++)
+            for(int j = 0; j<m; j++)
+                if(!vis[i][j] && grid[i][j]=='1') {
+                    bfs(i, j, vis, grid);
+                    ans++;
+                }
+        return ans;
+    }
+};
