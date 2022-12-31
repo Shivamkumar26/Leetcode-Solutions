1119eb6807b1cc8b4f3c1af390483d050daf96bf
Shivam kumar <99862619+Shivamkumar26@users.noreply.github.com>
2022-12-31 16:20:45 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (64.25%) - LeetHub

--- 0980-unique-paths-iii/0980-unique-paths-iii.cpp
+++ 0980-unique-paths-iii/0980-unique-paths-iii.cpp
@@ -1,36 +1,41 @@
 class Solution {
-    void solve(int i, int j, int count, int& ans, vector<vector<int>>& nums) {
-        if(i<0 || j<0 || i>=nums.size() || j>=nums[0].size() || nums[i][j]==-1) return;
-        if(nums[i][j]==2) {
-            if((count-1)==0) ans++;
+    void solve(int si, int sj, int count, int& ans, vector<vector<int>>& nums) {
+        if(si < 0 || sj < 0 || si == nums.size() || sj == nums[0].size() || 
+           nums[si][sj] == -1) 
+            return;
+        if(nums[si][sj] == 2) {
+            if(count == 0) ans++;
             return;
         }
         
-        nums[i][j] = -1;
-        solve(i+1, j, count-1, ans, nums);
-        solve(i, j+1, count-1, ans, nums);
-        solve(i-1, j, count-1, ans, nums);
-        solve(i, j-1, count-1, ans, nums);
-        nums[i][j] = 1;
+        nums[si][sj] = -1;
+        int dr[] = {0, 0, 1, -1};
+        int dc[] = {-1, 1, 0, 0};
+        for(int i=0; i<4; i++) 
+            solve(si+dr[i], sj+dc[i], count-1, ans, nums);
+        
+        nums[si][sj] = 1;
     }
 public:
     int uniquePathsIII(vector<vector<int>>& nums) {
-        int n = nums.size(), m = nums[0].size();
-        int count = 0, si = -1, sj = -1;
+        int n = nums.size();
+        int m = nums[0].size();
+        int ans = 0, count = 0;
+        int si = -1, sj = -1;
         
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<m; j++) {
-                if(nums[i][j]==0 || nums[i][j]==2)
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(nums[i][j] == 0 || nums[i][j] == 2)
                     count++;
-                else if(nums[i][j]==1) {
+                else if(nums[i][j] == 1) {
                     si = i;
                     sj = j;
                     count++;
                 }
             }
         }
-        int ans = 0;
-        solve(si, sj, count, ans, nums);
+        
+        solve(si, sj, count-1, ans, nums);
         return ans;
     }
 };
