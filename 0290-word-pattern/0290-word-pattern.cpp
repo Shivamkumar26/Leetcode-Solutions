55cacde867403fc6d6349a61f8a6d276eea718d1
Shivam kumar <99862619+Shivamkumar26@users.noreply.github.com>
2023-01-01 19:05:50 +0530

    Time: 2 ms (52.37%), Space: 6.4 MB (82.12%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.cpp
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -4,31 +4,20 @@ public:
         unordered_map <char, string> mp;
         unordered_map <string, char> mp2;
         
-        int n = p.size(), m = s.size();
-        int j=0;
+        int n = p.size(), m = s.size(), j=0;
         
         for(int i=0; i<n; i++) {
             string st = "";
             if(j>=m) return false;
             while(j<m && s[j]!=' ') st = st+s[j], j++;
             j++;
-            if(mp.find(p[i]) == mp.end()){   
-                mp[p[i]] = st;
-            }
-            else {
-                if(st == mp[p[i]]) continue;
-                return false;
-            }
             
-            if(mp2.find(st) == mp2.end()){   
-                mp2[st] = p[i];
-            }
-            else {
-                if(p[i] == mp2[st]) continue;
-                return false;
-            }
+            if(mp.find(p[i]) == mp.end()) mp[p[i]] = st;
+            else if(st != mp[p[i]]) return false;
+            
+            if(mp2.find(st) == mp2.end()) mp2[st] = p[i];
+            else if(p[i] != mp2[st]) return false;
         }
-        if(j<m) return false;
-        return true;
+        return j<m ? false : true;
     }
 };
