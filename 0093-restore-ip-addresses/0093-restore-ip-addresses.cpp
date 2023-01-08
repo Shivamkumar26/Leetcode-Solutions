7ccddfb060ed47a53dc18531d07ba8281c13a8da
Shivam kumar <99862619+Shivamkumar26@users.noreply.github.com>
2023-01-08 19:04:13 +0530

    Time: 6 ms (34.85%), Space: 6.6 MB (43.10%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+    void solve(int ind, string cur, int ip_count, string& s, vector<string>& ans) {
+        if(ip_count > 4) return;
+        if(ip_count == 4 && ind == s.size()) 
+            ans.push_back(cur);
+        
+        for(int i=0; i<3; i++) {
+            if(ind+i >= s.size()) break;
+            string temp = s.substr(ind, i+1);
+            if((temp[0] == '0' && temp.size() > 1) || (stoi(temp) >= 256))
+                continue;
+            solve(ind+i+1, cur + temp + (ip_count==3 ? "" : "."), ip_count+1, s, ans);
+        }
+    }
+public:
+    vector<string> restoreIpAddresses(string s) {
+        vector<string> ans;
+        solve(0, "", 0, s, ans);
+        return ans;
+    }
+};
