27d22adb4896cb49d6c5d5aaf02d84be85592969
Shivam kumar <99862619+Shivamkumar26@users.noreply.github.com>
2023-01-21 15:26:30 +0530

    Time: 6 ms (30.47%), Space: 6.5 MB (70.83%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
@@ -1,21 +1,21 @@
 class Solution {
-    void solve(int ind, string cur, int ip_count, string& s, vector<string>& ans) {
+    void solve(int ind, int ip_count, string cur, string& s, vector<string>& ans) {
         if(ip_count > 4) return;
-        if(ip_count == 4 && ind == s.size()) {
-            ans.push_back(cur); return; }
+        if(ip_count == 4 && s.size() == ind)
+            ans.push_back(cur);
         
         for(int i=0; i<3; i++) {
-            if(ind+i >= s.size()) break;
+            if(ind+i >= s.size()) return;
             string temp = s.substr(ind, i+1);
-            if((temp[0] == '0' && temp.size() > 1) || (stoi(temp) >= 256))
+            if((temp[0]=='0' && temp.size()>1) || (stoi(temp) >= 256))
                 continue;
-            solve(ind+i+1, cur + temp + (ip_count==3 ? "" : "."), ip_count+1, s, ans);
+            solve(ind+i+1, ip_count+1, cur + temp + (ip_count==3 ? "" : "."), s, ans);
         }
     }
 public:
     vector<string> restoreIpAddresses(string s) {
         vector<string> ans;
-        solve(0, "", 0, s, ans);
+        solve(0, 0, "", s, ans);
         return ans;
     }
 };
