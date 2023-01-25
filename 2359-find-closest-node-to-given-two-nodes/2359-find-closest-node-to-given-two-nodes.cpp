039bad9661eeb235de3802eb73da9df9efcafce9
Shivam kumar <99862619+Shivamkumar26@users.noreply.github.com>
2023-01-25 21:04:46 +0530

    Time: 479 ms (31.30%), Space: 168.6 MB (13.57%) - LeetHub

--- 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.cpp
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.cpp
@@ -1,29 +1,32 @@
 class Solution {
-public:
-    void dfs(int i, vector<int> &dist ,vector<int> &vis,vector<int> adj[],int dis){
-        vis[i]=1;
-        dist[i]=dis;
-        for(int &child:adj[i]){
-            if(vis[child]==1)continue;
-            dfs(child,dist,vis,adj,dis+1);
+    void dfs(int node, vector<int>& dist, vector<int>& vis, vector<int> adj[], int d) {
+        vis[node] = 1;
+        dist[node] = d;
+        for(int &child : adj[node]) {
+            if(vis[child] == 1) continue;
+            dfs(child, dist, vis, adj, d+1);
         }
-    } 
+    }
+public:
     int closestMeetingNode(vector<int>& edges, int node1, int node2) {
         int n = edges.size();
         vector<int> adj[n];
-        for(int i=0; i<n; i++){
-            if(edges[i] != -1){
-                adj[i].push_back(edges[i]);
-            }
-        }
-        vector<int> distn1(n,1e9), distn2(n,1e9);
-        vector<int> vis1(n,0), vis2(n,0);
-        dfs(node1,distn1,vis1,adj,0);
-        dfs(node2,distn2,vis2,adj,0);
-        int ans=1e9, node=-1;
-        for(int i=0; i<n; i++){
-            if(max(distn1[i],distn2[i]) < ans){
-                ans = max(distn1[i],distn2[i]);
+        //making adjacent connection
+        for(int i = 0; i < n; i++)
+            if(edges[i] != -1) adj[i].push_back(edges[i]);
+        
+        vector<int> dist1(n, INT_MAX),  dist2(n, INT_MAX); 
+        vector<int> vis1(n, 0),         vis2(n, 0);
+        
+        //simple dfs call
+        dfs(node1, dist1, vis1, adj, 0);
+        dfs(node2, dist2, vis2, adj, 0);   
+        
+        //finding max ko minimize
+        int ans = INT_MAX, node = -1;
+        for(int i = 0; i < n; i++) {
+            if(max(dist1[i], dist2[i]) < ans) {
+                ans = max(dist1[i], dist2[i]);
                 node = i;
             }
         }
