6b8c0531dfeb8c1c37d301f99cd6ca37a12024d9
Shivam kumar <99862619+Shivamkumar26@users.noreply.github.com>
2023-01-26 10:36:14 +0530

    Added solution - LeetHub

---
+++ Case-specific Sorting of Strings - GFG/casespecific-sorting-of-strings.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+    //Function to perform case-specific sorting of strings.
+    string caseSort(string str, int n)
+    {
+        string upper = "";
+        string lower = "";
+        
+        for(int i=0; i<n; i++) {
+            if(str[i]>='A' && str[i]<='Z') upper+=str[i];
+            else lower+=str[i];
+        }
+        
+        sort(upper.begin(), upper.end());
+        sort(lower.begin(), lower.end());
+        int p1=0, p2=0;
+        for(int i=0; i<n; i++) {
+            if(str[i]>='A' && str[i]<='Z') str[i]=upper[p1++];
+            else str[i]=lower[p2++];
+        }
+        return str;
+    }
+};
+
+/* 
+
+S =     defRTSersUXI
+Output: deeIRSfrsTUX
+
+*/
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		string str;
+		cin>>str;
+		Solution obj;
+		cout<<obj.caseSort (str, n)<<endl;
+	}
+}
+// } Driver Code Ends
