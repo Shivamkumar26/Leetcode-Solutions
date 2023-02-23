d9558fc6db7e53682a2ef73ce6bb4edc15507cd5
Shivam kumar <99862619+Shivamkumar26@users.noreply.github.com>
2023-02-23 20:38:55 +0530

    Time: 7 ms (89.11%), Space: 13.9 MB (93.54%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,29 +1,24 @@
 class Solution {
-    void dfs(int r, int c, int matchColor, int newColor, 
-    vector<vector<int>>& nums, vector<vector<int>>& vis) {
-        int n = nums.size();
-        int m = nums[0].size();
-        if(r>=0 && c>=0 && r<n && c<m 
-        && nums[r][c]==matchColor && !vis[r][c]) {
-            nums[r][c] = newColor;
-            vis[r][c]=1;
-        }
-        else return;
-        int dr[] = {-1, 1, 0, 0};
-        int dc[] = {0, 0, 1, -1};
-        for(int i=0; i<4; i++) {
-            int row = r + dr[i];
-            int col = c + dc[i];
-            dfs(row, col, matchColor, newColor, nums, vis);
+    void dfs(int r, int c, vector<vector<int>>& image, int beforeColor, int afterColor) {
+        int n = image.size(), m = image[0].size();
+        image[r][c] = afterColor;
+        int dr[] = {1, 0, -1, 0};
+        int dc[] = {0, 1, 0, -1};
+        
+        for(int i = 0; i < 4; i++) {
+            int cr = r + dr[i];    
+            int cc = c + dc[i];    
+            if(cr >=0 && cr < n && cc >=0 && cc < m && image[cr][cc]==beforeColor && image[cr][cc]!=afterColor) {
+                dfs(cr, cc, image, beforeColor, afterColor);
+            }
         }
     }
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& nums, int sr, int sc, int newColor) {
-        int n = nums.size();
-        int m = nums[0].size();
-        int matchColor = nums[sr][sc];
-        vector<vector<int>> vis(n, vector<int>(m, 0));
-        dfs(sr, sc, matchColor, newColor, nums, vis);
-        return nums;
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int n = image.size(), m = image[0].size();
+        int colorToChange = image[sr][sc];
+        
+        dfs(sr, sc, image, colorToChange, color);
+        return image;
     }
 };
