522621422636d3181ba85bdc3064f445e06a438e
Shivam kumar <99862619+Shivamkumar26@users.noreply.github.com>
2023-02-23 20:58:21 +0530

    Time: 3 ms (96.47%), Space: 13 MB (85.40%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,42 +1,41 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
+        int n = grid.size(), m = grid[0].size();
         queue<pair<int, int>> q;
         
-        for(int i = 0; i < n; i++) 
-            for(int j = 0; j < m; j++) 
-                if(grid[i][j] == 2) q.push({i, j});
+        for(int i=0; i<n; i++) {
+            for(int j = 0; j<m; j++) {
+                if(grid[i][j]==2) q.push({i, j});
+            }
+        }
+        int minutes = -1;
+        int dr[] = {-1, 1, 0, 0};
+        int dc[] = {0, 0, -1, 1};
         
-        int dr[] = {1, 0, 0, -1};
-        int dc[] = {0, 1, -1, 0};
-        
-        int ans = -1;
-        while(q.size()) {
+        while(!q.empty()) {
             int sz = q.size();
-            ans++;
-            for(int i = 0; i < sz; i++) {
+            minutes++;
+            for(int i=0; i<sz; i++) {
                 int r = q.front().first;
                 int c = q.front().second;
                 q.pop();
                 
-                for(int d=0; d<4; d++) {
-                    int new_r = r + dr[d];
-                    int new_c = c + dc[d];
-                    
-                    if(new_r >= 0 && new_c >= 0 && new_r < n && new_c < m
-                      && grid[new_r][new_c]==1) {
-                        q.push({new_r, new_c});
-                        grid[new_r][new_c] = 2;
+                for(int j = 0; j < 4; j++) {
+                    int nr = r + dr[j];
+                    int nc = c + dc[j];
+                    if(nr >= 0 && nr < n && nc >=0 && nc < m && grid[nr][nc]==1) {
+                        grid[nr][nc] = 2;
+                        q.push({nr, nc});
                     }
                 }
             }
         }
-        for(int i=0; i<n; i++) for(int j=0; j<m; j++)
-            if(grid[i][j]==1) return -1;
-        if(ans == -1) return 0;
-
-        return ans;
+        
+        for(int i=0; i<n; i++) 
+            for(int j=0; j<m; j++) 
+                if(grid[i][j]==1) return -1;
+        if(minutes==-1) return 0;
+        return minutes;
     }
 };
