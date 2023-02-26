6019f7de4dc4c1f1fe77493830fdb190c698219c
Shivam kumar <99862619+Shivamkumar26@users.noreply.github.com>
2023-02-26 19:00:11 +0530

    Time: 11 ms (89.43%), Space: 9.9 MB (96.23%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,43 +1,30 @@
 class Solution {
-    void dfs(int r, int c, vector<vector<char>>& grid, vector<vector<int>>& vis) {
-        int n = grid.size(), m = grid[0].size();
-        if(r<0 || c<0 || r>n-1 || c>m-1 || vis[r][c]==1 || grid[r][c]=='X') 
-        return;
+public:
+     void dfs(int i, int j, vector<vector<char>>& mat) {
+        if(i<0 || j<0 || i>=mat.size() || j>=mat[0].size()) return;
+        if(mat[i][j]=='X' || mat[i][j]=='-') return;
         
-        vis[r][c]=1;
-        int dr[] = {0, 0, -1, 1};
-        int dc[] = {-1, 1, 0, 0};
-        
-        for(int i=0; i<4; i++){
-            int cr = r+dr[i];
-            int cc = c+dc[i];
-            dfs(cr, cc, grid, vis);
-        }
+        mat[i][j]='-';
+        dfs(i+1, j, mat);
+        dfs(i-1, j, mat);
+        dfs(i, j+1, mat);
+        dfs(i, j-1, mat);
     }
 public:
-    void solve(vector<vector<char>>& grid) {
-        int n = grid.size(), m = grid[0].size();
-        vector<vector<int>> vis(n, vector<int>(m, 0));
-        
-        //making dfs calls on boundaries
-        for(int i=0; i<m; i++) {
-            if(!vis[0][i] && grid[0][i]=='O') 
-                dfs(0, i, grid, vis);
-            if(!vis[n-1][i] && grid[n-1][i]=='O')
-                dfs(n-1, i, grid, vis);
-        }
-        for(int i=0; i<n; i++) {
-            if(!vis[i][0] && grid[i][0]=='O') 
-            dfs(i, 0, grid, vis);
-            if(!vis[i][m-1] && grid[i][m-1]=='O')
-            dfs(i, m-1, grid, vis);
-        }
-        
-        //making the changes in the grid.
+    void solve(vector<vector<char>>& mat)
+    {
+        int n = mat.size(), m = mat[0].size();
         for(int i=0; i<n; i++) {
             for(int j=0; j<m; j++) {
-                if(!vis[i][j] && grid[i][j]=='O')
-                grid[i][j]='X';
+                if((i==0 || j==0 || i==n-1 || j==m-1) && mat[i][j]=='O'){
+                    dfs(i, j, mat);
+                }
+            }
+        }
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                if(mat[i][j] == '-') mat[i][j] = 'O';
+                else mat[i][j] = 'X';
             }
         }
     }
