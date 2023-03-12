da0a93e3bc9cbf42cc024849c03efa6ece7652ce
Shivam kumar <99862619+Shivamkumar26@users.noreply.github.com>
2023-03-12 20:25:25 +0530

    Added solution - LeetHub

---
+++ Binary matrix having maximum number of 1s - GFG/binary-matrix-having-maximum-number-of-1s.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+public:
+    vector<int> findMaxRow(vector<vector<int>> mat, int n) {
+       int ans = 0, ones = 0;
+       for(int i=0; i<n; i++) {
+           int ind =  lower_bound(mat[i].begin(), mat[i].end(), 1) - mat[i].begin();
+           int curr = n-ind;
+           if(ones < curr) {
+               ones = curr;
+               ans = i;
+           }
+       }
+       return {ans, ones};
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	int t;
+    cin>>t;
+    while(t--) {
+        int n;
+        cin>>n;
+        vector<vector<int>> arr(n, vector<int> (n));
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                cin >> arr[i][j];
+        Solution obj;
+        vector<int> ans = obj.findMaxRow(arr, n);
+        for(int val : ans) {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+}
+// } Driver Code Ends
