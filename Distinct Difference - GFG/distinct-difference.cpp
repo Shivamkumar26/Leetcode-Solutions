df4c2b34e7da2beaa133d967453a47cd6ce5805c
Shivam kumar <99862619+Shivamkumar26@users.noreply.github.com>
2023-03-19 21:21:16 +0530

    Added solution - LeetHub

--- Distinct Difference - GFG/distinct-difference.cpp
+++ Distinct Difference - GFG/distinct-difference.cpp
@@ -32,39 +32,24 @@ class Solution {
   public:
     vector<int> getDistinctDifference(int n, vector<int> &arr) {
         // code here
-        vector<int> v1(n), v2(n);
+        vector<int> v1(n), v2(n), ans(n);
         set<int> s1, s2;
         
         for(int i=0; i<n; i++) {
-            int sz1 = s1.size();
+            int sz1 = s1.size(), sz2 = s2.size();;
             s1.insert(arr[i]);
-            if(sz1==s1.size()) 
-                v1[i] = v1[i-1];
-            else {
-                if(i==0) v1[i] = 1;
-                else v1[i] = v1[i-1]+1;
-            }
-        }
-        for(int i=n-1; i>=0; i--) {
-            int sz2 = s2.size();
-            s2.insert(arr[i]);
-            if(sz2==s2.size()) 
-                v2[i] = v2[i+1];
-            else {
-                if(i==n-1) v2[i] = 1;
-                else v2[i] = v2[i+1]+1;
-            }
+            if(sz1==s1.size()) v1[i] = v1[i-1];
+            else v1[i] = (i==0) ? 1 : v1[i-1]+1;
+            
+            s2.insert(arr[n-1-i]);
+            if(sz2==s2.size()) v2[n-1-i] = v2[n-i];
+            else v2[n-1-i] = (i==0) ? 1 : v2[n-i]+1;
         }
         
-        vector<int> ans(n);
         for(int i=0; i<n; i++) {
-            //cout << v1[i] << " " << v2[i] << endl;
-            int a = 0;
-            if(i==0) a=0;
-            else a = v1[i-1];
-            int b = 0;
-            if(i==n-1) b=0;
-            else b = v2[i+1];
+            int a = 0, b=0;
+            a = (i==0) ? 0 : v1[i-1];
+            b = (i==n-1) ? 0 : v2[i+1];
             ans[i] =  a-b;
         }
         return ans;    
