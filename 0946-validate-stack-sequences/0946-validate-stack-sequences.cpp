d33fba60608d9e04e783efb26bc64be6b8836d1a
Shivam kumar <99862619+Shivamkumar26@users.noreply.github.com>
2023-04-13 19:05:36 +0530

    Time: 11 ms (52.74%), Space: 15.4 MB (24.28%) - LeetHub

--- 0946-validate-stack-sequences/0946-validate-stack-sequences.cpp
+++ 0946-validate-stack-sequences/0946-validate-stack-sequences.cpp
@@ -6,12 +6,12 @@ public:
         int i=0, j=0;
         while (i<n) {
             s.push(a1[i]);
-            while (j<n && s.size() && s.top()==a2[j]) {
+            while (s.size() && s.top()==a2[j]) {
                 s.pop();
                 j++;
             }
             i++;
         }
-        return (s.size()==0) && (i>=n-1) && (j>=n-1) ? true : false;
+        return (s.size()==0) ? true : false;
     }
 };
