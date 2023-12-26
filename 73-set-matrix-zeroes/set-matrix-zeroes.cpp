e3a415c9d515fd0b52ced351b84eb3688c4ab60d
Shivam kumar <99862619+Shivamkumar26@users.noreply.github.com>
2023-12-26 13:48:30 +0530

    Time: 8 ms (87.84%) | Memory: 13.8 MB (78.29%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -4,16 +4,8 @@ public:
         int n = grid.size(), m = grid[0].size();
         vector<int> r0(m, 1), c0(n, 1);
         
-        if(grid[0][0]==0) r0[0]=0, c0[0]=0;
-        for(int i=1; i<m; i++) {
-            if(grid[0][i]==0) r0[i]=0, c0[0]=0;
-        }
-        for(int i=1; i<n; i++) {
-            if(grid[i][0]==0) c0[i]=0, r0[0]=0;
-        }
-        
-        for(int i=1; i<n; i++) {
-            for(int j=1; j<m; j++) {
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
                 if(grid[i][j]==0) r0[j]=0, c0[i]=0;
             }
         }
@@ -23,14 +15,5 @@ public:
                 if(r0[j]==0 || c0[i]==0) grid[i][j] = 0;
             }
         }
-        
-        for(int i=0; i<n; i++) {
-            cout << c0[i] << " ";
-        }
-        cout << "\n";
-        for(int j=0; j<m; j++) {
-            cout << r0[j] << " ";
-        }
-        //return grid;
     }
 };
