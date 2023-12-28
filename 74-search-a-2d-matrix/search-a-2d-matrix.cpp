a682082c28424db4a4874690b9a6d06bbb805bea
Shivam kumar <99862619+Shivamkumar26@users.noreply.github.com>
2023-12-28 13:03:33 +0530

    Time: 13 ms (19.40%) | Memory: 10 MB (24.04%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -4,7 +4,7 @@ public:
         int l = 0, r = mat.size()*mat[0].size()-1, m = mat[0].size();
         while(l!=r) {
             // int mid = l + (r-l)/2;
-            int mid = (l+r-1)/2;
+            int mid = (l+r)/2;
             if(mat[mid / m][mid % m] == target) return 1;
             if(mat[mid / m][mid % m] < target)
                 l = mid+1;
