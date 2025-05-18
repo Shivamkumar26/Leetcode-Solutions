850a2b3aa666db56132dc565b2aab472cc4bf224
Shivam kumar <99862619+Shivamkumar26@users.noreply.github.com>
2025-05-19 01:05:23 +0530

    Time: 1195 ms (5.01%) | Memory: 47.3 MB (42.03%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -1,11 +1,11 @@
 class Solution {
-    vector<int> leftNearestSmallestElementIndex(vector<int>& nums) {
+    vector<int> nextSmallRight(vector<int>& nums) {
         int n = nums.size();
-        vector<int> res(n, -1);
         stack<int> st;
+        vector<int> res(n, n);
 
-        for(int i=0; i<n; i++) {
-            while(st.size() && nums[st.top()] >= nums[i]) {
+        for(int i=n-1; i>=0; i--) {
+            while(st.size() && nums[st.top()]>=nums[i]) {
                 st.pop();
             }
             if(st.size()) res[i] = st.top();
@@ -15,18 +15,19 @@ class Solution {
         return res;
     }
 
-     vector<int> rightNearestSmallestElementIndex(vector<int>& nums) {
+    vector<int> nextSmallLeft(vector<int>& nums) {
         int n = nums.size();
-        vector<int> res(n, n);
         stack<int> st;
+        vector<int> res(n, -1);
 
-        for(int i=n-1; i>=0; i--) {
-            while(st.size() && nums[st.top()] > nums[i]) {
+        for(int i=0; i<n; i++) {
+            while(st.size() && nums[st.top()]>nums[i]) {
                 st.pop();
             }
             if(st.size()) res[i] = st.top();
             st.push(i);
         }
+
         return res;
     }
     int mod = 1e9+7;
@@ -35,9 +36,17 @@ public:
         int n = nums.size();
         long long ans = 0;
 
-        vector<int> lns = leftNearestSmallestElementIndex(nums);
-        vector<int> rns = rightNearestSmallestElementIndex(nums);
+        vector<int> lns = nextSmallLeft(nums);
+        vector<int> rns = nextSmallRight(nums);
 
+
+        for(int i=0; i<n; i++) {
+            cout << lns[i] << " ";
+        }
+        cout << endl;
+for(int i=0; i<n; i++) {
+            cout << rns[i] << " ";
+        }
         for(int i=0; i<n; i++) {
             long long lt = i - lns[i];
             long long rt = rns[i] - i;
